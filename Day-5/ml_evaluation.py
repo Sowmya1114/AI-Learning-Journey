bef1a8e4302fc267227450ff92ec600f5544f7ef
Sowmya1114 <sabbavarapussri@gmail.com>
2026-01-26 22:39:48 +0530

    ML evaluation code added

---
+++ Day-5/ml_evaluation.py
@@ -0,0 +1,20 @@
+# Day-5 ML Model Evaluation
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
